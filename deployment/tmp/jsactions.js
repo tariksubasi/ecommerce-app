d411f005507d129c5f85e4a40fbbb5d2a04d4fe7
tarik.subasi <tarik.subasi@hotmail.com>
2021-10-17 21:50:19 +0300

    13 - Adding progress bar to nanoflow

---
+++ deployment/tmp/jsactions.js
@@ -0,0 +1,2 @@
+export const NanoflowCommons$ShowProgress = async () => (await import("C:/Users/tarik/Documents/Mendix/ecommerce-app-main/javascriptsource/nanoflowcommons/actions/ShowProgress")).ShowProgress;
+export const NanoflowCommons$HideProgress = async () => (await import("C:/Users/tarik/Documents/Mendix/ecommerce-app-main/javascriptsource/nanoflowcommons/actions/HideProgress")).HideProgress;
