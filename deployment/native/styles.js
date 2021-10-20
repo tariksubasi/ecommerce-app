80aef46857945b0112e9275ef678e8df1a5768e2
tarik.subasi <tarik.subasi@hotmail.com>
2021-10-20 21:41:51 +0300

    18 - Prevent User to Access Admin Panel and Role Based Home Pages

--- deployment/native/styles.js
+++ deployment/native/styles.js
@@ -4,8 +4,9 @@ import * as style2 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/them
 import * as style3 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/themesource/atlas_core/native/main";
 import * as style4 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/themesource/admin/native/main";
 import * as style5 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/themesource/anonymous/native/main";
-import * as style6 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/theme/native/main";
+import * as style6 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/themesource/products/native/main";
+import * as style7 from "C:/Users/tarik/Documents/Mendix/ecommerce-app-main/theme/native/main";
 
 import { flatten } from "mendix/native";
 
-module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7]);
