d427746a8289e00a40d8f75565f7a1a80a4f237e
tarik.subasi <tarik.subasi@hotmail.com>
2021-10-31 18:19:27 +0300

    57 - Manipulating UI with Mendix Client API

--- deployment/tmp/jsactions.js
+++ deployment/tmp/jsactions.js
@@ -1,3 +1,3 @@
+export const SalesManager$JS_OpenSharePage = async () => (await import("C:/Users/tarik/Documents/Mendix/ecommerce-app-main/javascriptsource/salesmanager/actions/JS_OpenSharePage")).JS_OpenSharePage;
 export const NanoflowCommons$ShowProgress = async () => (await import("C:/Users/tarik/Documents/Mendix/ecommerce-app-main/javascriptsource/nanoflowcommons/actions/ShowProgress")).ShowProgress;
-export const SalesManager$JS_ThrowRandomError = async () => (await import("C:/Users/tarik/Documents/Mendix/ecommerce-app-main/javascriptsource/salesmanager/actions/JS_ThrowRandomError")).JS_ThrowRandomError;
 export const NanoflowCommons$HideProgress = async () => (await import("C:/Users/tarik/Documents/Mendix/ecommerce-app-main/javascriptsource/nanoflowcommons/actions/HideProgress")).HideProgress;
